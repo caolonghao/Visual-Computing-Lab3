85e1a2d532d3f4c155a94e514f9fa3ebfa5c68d8
Tony Cao <tonycao0918@qq.com>
2022-05-02 14:19:10 +0800

    smooth complete

--- smooth.py
+++ smooth.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sys
 import argparse
+
+from sklearn import neighbors
 from base_model import MeshModel
 
 # Mesh smooth calss
@@ -12,14 +14,22 @@ class mesh_smooth(MeshModel):
     # 找到每个顶点的所有邻居
     def find_vertex_neighbors(self):
         vertex_neighbors = []
-        pass
+        for i in range(self.numb_v):
+            valid_edge = np.nonzero(self.edges[0] == i)[0]
+            tmp_neighbors = self.edges[1][valid_edge]
+            vertex_neighbors.append(tmp_neighbors)
         return vertex_neighbors
     
     # 根据邻接关系计算用于平滑的laplacian矩阵
     def laplacian_calculation(self):
         v_neighbors = self.find_vertex_neighbors()
-        lap_matrix = None
-        pass
+        lap_matrix = []
+        for i in range(self.numb_v):
+            tmp = np.zeros(self.numb_v)
+            num_neibors = len(v_neighbors[i])
+            tmp[i] = -num_neibors
+            tmp[v_neighbors] = 1
+            tmp /= num_neibors
         return lap_matrix
     
     # 用计算的laplacian矩阵对顶点位置进行平滑，iterations为迭代次数，lamb是每次迭代的平滑程度
@@ -27,7 +37,7 @@ class mesh_smooth(MeshModel):
     def filter_laplacian(self, iterations: int, lamb:float):
         lap_mat = self.laplacian_calculation()
         for _ in range(iterations):
-            pass
+            self.vertices = lamb * (np.matmul(lap_mat, self.vertices)) + (1 - lamb) * self.vertices
 
 
 if __name__ =="__main__":

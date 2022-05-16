26fcf39ea8426c87678a3ff147c0727a9192289d
Tony Cao <tonycao0918@qq.com>
2022-05-16 15:17:09 +0800

    bug fixed

--- subdivision.py
+++ subdivision.py
@@ -14,17 +14,18 @@ class mesh_subdivision(MeshModel):
     # 根据边中点进行细分
     # 找到所有边中点的位置，将所有中点拼接在self.vertices中，再修改所有三角面片，1个三角面片被细分成4个三角形
     def subdivide_by_mid(self):
+        # print("edge_num: ", self.edges.shape[0])
         out_faces = []
         center_dict = {}
         for edge in self.edges:
             p1_id, p2_id = edge
-            p1, p2 = self.vertices[edge]
-            center_dict[(p1_id, p2_id)] = self.vertices.shape[0]
-            center_dict[(p2_id, p1_id)] = self.vertices.shape[0]
+            p1, p2 = self.vertices[p1_id - 1], self.vertices[p2_id - 1]
             p_center = (p1 + p2) / 2
             p_center = p_center.reshape(1, -1)
-            self.vertices = np.concatenate(self.vertices, p_center)
-        
+            self.vertices = np.concatenate((self.vertices, p_center))
+            center_dict[(p1_id, p2_id)] = self.vertices.shape[0] #新的点的编号恰等于现在的点个数
+        self.numb_v = self.vertices.shape[0]
+
         for face in self.faces:
             p1_id, p2_id, p3_id = face
             c1_id = center_dict[ (p2_id, p3_id) ]
@@ -36,14 +37,16 @@ class mesh_subdivision(MeshModel):
             out_faces.append([c1_id, c2_id, c3_id])
         
         self.faces = np.array(out_faces)
+        self.numb_f = self.faces.shape[0]
 
 if __name__ =="__main__":
 
     parser=argparse.ArgumentParser(description='Mesh subdivision')
-    parser.add_argument('-i', type=str, default='models/kitten.obj', help='input file path of an existing 3d model.')
-    parser.add_argument('-o', type=str, default='results/subdivision_kitten.obj', help='output path of 3d model.')
+    parser.add_argument('-i', type=str, default='models/block.obj', help='input file path of an existing 3d model.')
+    parser.add_argument('-o', type=str, default='results/subdivision_block.obj', help='output path of 3d model.')
     args=parser.parse_args()
 
     model = mesh_subdivision(args.i)
+    print("subdivision started!")
     model.subdivide_by_mid()
     model.save_obj(args.o)
